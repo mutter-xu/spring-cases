7fb0f83929dbf805cf1189a80423a21ac9a972f7
kongtan <2453875322@qq.com>
2021-05-08 23:00:43 +0800

    SpringCases项目初始化

---
+++ src/main/java/org/kongtan/source/springcases/SpringCasesApplication.java
@@ -0,0 +1,13 @@
+package org.kongtan.source.springcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCasesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kongtan/source/springcases/SpringCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kongtan.source.springcases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
